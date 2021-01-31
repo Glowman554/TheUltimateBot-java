083cffa7cbd555dedf182cb01fd90001d6f137aa
janick voss <vossjanick@gmail.com>
2021-01-31 14:38:18 +0100

    added chatbot

--- src/main/java/com/glowman554/bot/Main.java
+++ src/main/java/com/glowman554/bot/Main.java
@@ -9,8 +9,13 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 public class Main extends ListenerAdapter {
+
+    public static ChatBot bot = new ChatBot();
+
     public static void main(String[] args) throws Exception {
         String token = Utils.getTokenFromFile("token.txt");
+
+        bot.setup("alice", "resources");
 
         JDABuilder jdaBuilder =  JDABuilder.createDefault(token);
         JDA jda = jdaBuilder.build();
@@ -22,18 +27,27 @@ public class Main extends ListenerAdapter {
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         super.onMessageReceived(event);
 
+        MessageChannel channel = event.getChannel();
+
         if(event.isFromType(ChannelType.PRIVATE)) {
             System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(), event.getMessage().getContentDisplay());
         } else {
             System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(), event.getTextChannel().getName(), event.getMember().getEffectiveName(), event.getMessage().getContentDisplay());
         }
 
+        if(event.getAuthor().isBot()) {
+            return;
+        }
+
+        if(event.isFromType(ChannelType.PRIVATE) || event.getTextChannel().getName().contains("chatbot")) {
+            channel.sendMessage(bot.get_answer(event.getMessage().getContentDisplay())).queue();
+            return;
+        }
+
         if(event.getMessage().getContentRaw().contains("\uD83D\uDD25")) {
             event.getMessage().addReaction("\uD83D\uDE92").queue();
             event.getMessage().addReaction("\uD83E\uDDEF").queue();
         }
-
-        MessageChannel channel = event.getChannel();
 
         if(event.getMessage().getContentRaw().equals("!ping")) {
             channel.sendMessage("Pong!").queue();
